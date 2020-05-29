ecb0d31cb977c2a6c41a478ad9195d51e016b5ad
Lukas Knuth <mr.luke.187@googlemail.com>
2020-05-29 20:19:22 +0200

    Implemented automatic channel registration with router. Implemented handling incoming annoyences.

--- lib/annoyer/router.ex
+++ lib/annoyer/router.ex
@@ -1,14 +1,36 @@
 defmodule Annoyer.Router do
     use GenServer
 
+    # todo Implement with maps NOW, update to ETS after
+
     @impl true
-    def init(:ok) do
-        {:ok, %{}}
+    def init(_params) do
+        channels = case :application.get_key(:annoyer, :modules) do
+            {:ok, modules} -> Enum.filter(modules, &is_channel?/1)
+            _ -> raise "Couldn't list Apps modules!"
+        end
+
+        modules = Enum.reduce(channels, %{}, fn channel, acc ->
+           Enum.reduce(channel.__subscribed_topics__, acc, fn topic, routes ->
+               Map.update(routes, topic, [channel], &([channel | &1]))
+           end)
+        end)
+        {:ok, modules}
+    end
+
+    defp is_channel?(module) do
+        Code.ensure_loaded?(module) and function_exported?(module, :__process_channel__, 1)
     end
 
     @impl true
-    def handle_call(_request, _from, state) do
-        {:reply, :ok, state}
+    def handle_call({:handle, annoyence}, _from, state) do
+        topic = annoyence.topic
+        case state do
+            %{^topic => channels} ->
+                Enum.each(channels, fn channel -> channel.__process_channel__(annoyence) end)
+                {:reply, :ok, state}
+            _ -> {:reply, :unknown, state}
+        end
     end
 
     ### CLIENT CODE
@@ -17,8 +39,8 @@ defmodule Annoyer.Router do
         GenServer.start_link(__MODULE__, default, name: Annoyer.Router)
     end
 
-    def do_something(pid) do
-        GenServer.call(pid, :something)
+    def process_incoming(annoyence) do
+        GenServer.call(Annoyer.Router, {:handle, annoyence})
     end
 
 end
