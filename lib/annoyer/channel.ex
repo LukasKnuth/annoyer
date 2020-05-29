1ca7be769f31225f39d4278744726c8fdc71270e
Lukas Knuth <mr.luke.187@googlemail.com>
2020-05-29 14:34:29 +0200

    Implemented channels-DSL via macros.

--- lib/annoyer/channel.ex
+++ lib/annoyer/channel.ex
@@ -1,18 +1,62 @@
 defmodule Annoyer.Channel do
-    
-    @doc "Imports any necessary modules for simple usage."
-    defmacro __using__(_opts) do
-        quote do
-            import Annoyer.Channel
-        end
-    end
 
-    def filter(filter) do
-        
-    end
+  @doc "Imports any necessary modules for simple usage."
+  defmacro __using__(_opts) do
+    quote do
+      import Annoyer.Channel
 
-    def outgoing(outgoing) do
-        
+      # Save filters and outgoings in module attributes
+      @filters []
+      @outgoings []
+
+      # Generate the final "__process_channel__"-method
+      @before_compile Annoyer.Channel
+
+      # Todo code-style: Create behaviour for Outgoing and Filter! says "Filter accepts map of arguemnts"
     end
+  end
+
+  defmacro filter(implementation, parameters \\ []) do
+    quote location: :keep do
+      with {:error, reason} <- Code.ensure_loaded(unquote(implementation)) do
+        raise "The specified module couldn't be loaded: #{reason}"
+      end
+      unless function_exported?(unquote(implementation), :filter, 2) do
+         raise "The specified module does not have the required filter/2 function!"
+      end
+
+      # Prepend this filter (Note: No = sign!)
+      # This is reversed below, since prepend is O(1) while append is O(n)
+      @filters [{unquote(implementation), unquote(parameters)} | @filters]
+    end
+  end
+
+  defmacro outgoing(implementation, parameters \\ []) do
+    quote location: :keep do
+      with {:error, reason} <- Code.ensure_loaded(unquote(implementation)) do
+        raise "The specified module couldn't be loaded: #{reason}"
+      end
+      unless function_exported?(unquote(implementation), :output, 2) do
+        raise "The specified module does not have the required output/2 function!"
+      end
+
+      @outgoings [{unquote(implementation), unquote(parameters)} | @outgoings]
+    end
+  end
+
+  # After defining all filters and outgoings, this finally creates the execute()-method to run everything
+  @doc false
+  defmacro __before_compile__(_env) do
+    quote location: :keep do
+      @filters_reversed Enum.reverse(@filters)
+
+      def __process_channel__(annoyence) do
+        # Execute all filters
+        filtered = Enum.reduce(@filters_reversed, annoyence, fn {filter, params}, acc -> filter.filter(params, acc) end)
+        # Execute all outgoings
+        Enum.each(@outgoings, fn {out, params} -> out.output(params, filtered) end)
+      end
+    end
+  end
 
 end
--- lib/channels/debug.ex
+++ lib/channels/debug.ex
@@ -1,5 +1,8 @@
 defmodule DebugChannel do
     use Annoyer.Channel
 
+    filter Annoyer.Filter.TextManipulate, prepend: "Hello, ", append: " and World"
+    filter Annoyer.Filter.TextManipulate, append: "..."
+
     outgoing Annoyer.Outgoing.Console
 end
