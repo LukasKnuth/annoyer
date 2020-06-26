693f5bfe800c2ec74fb146574a0a5778e3cb9981
Lukas Knuth <mr.luke.187@googlemail.com>
2020-06-26 13:34:16 +0200

    FIX: Path.get/2 helper now returns tuple as specified.

--- lib/annoyer/helpers/path.ex
+++ lib/annoyer/helpers/path.ex
@@ -1,14 +1,14 @@
 defmodule Annoyer.Helpers.Path do
 
-  @spec fetch(path :: keyword(), data:: any) :: {:ok, any} | :error
+  @spec fetch(path :: keyword(), data:: map) :: {:ok, any} | :error
   def fetch([key | rest], data) do
     with {:ok, found} <- Map.fetch(data, key),
       do: fetch(rest, found)
   end
 
-  def fetch([], data), do: data
+  def fetch([], data), do: {:ok, data}
 
-  @spec get(path :: keyword(), data :: any, fallback :: any) :: any
+  @spec get(path :: keyword(), data :: map, fallback :: any) :: any
   def get(path, data, fallback) do
     case fetch(path, data) do
       {:ok, result} -> result
