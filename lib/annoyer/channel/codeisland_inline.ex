aab3a348ce027e8138e93e08f5f1c64833fa3b59
Lukas Knuth <mr.luke.187@googlemail.com>
2020-07-10 15:02:04 +0200

    Annoyence attachments should map strings to values. Corrected JSON parser usage.
    Also implemented the ffmpeg filter based on the new Path helper.

--- lib/annoyer/channel/codeisland_inline.ex
+++ lib/annoyer/channel/codeisland_inline.ex
@@ -1,5 +1,6 @@
 defmodule Annoyer.Channel.CodeislandInline do
   use Annoyer.Channel
+  alias Annoyer.Helpers.Path
 
   configure(Annoyer.Http.Fetch,
     topic: "codeisland_inline",
@@ -12,15 +13,14 @@ defmodule Annoyer.Channel.CodeislandInline do
   transform Annoyer.Transform.JsonParser, body_path: [:attachments, :fetch_body], json_path: "$.data.*"
 
   def find_ffmpeg(annoyence) do
-    any = fn :get, data, next -> Enum.map(data, next) end
-    tags = get_in(annoyence.attachments, [:json, "tags", any])
-    Enum.all?(tags, fn tag -> tag != "ffmpeg" end)
+    Path.get([:attachments, "json", "tags"], annoyence, [])
+    |> Enum.all?(fn tag -> tag != "ffmpeg" end)
   end
   transform Annoyer.Transform.Drop, condition: &__MODULE__.find_ffmpeg/1
 
   # todo now, filter all that have already been published...
 
-  transform Annoyer.Transform.ToString, sources: [[:attachments, :json, "title"], [:attachments, :json, "url"]], join: " -> "
+  transform Annoyer.Transform.ToString, sources: [[:attachments, "json", "title"], [:attachments, "json", "url"]], join: " -> "
 
-  outgoing Annoyer.Outgoing.Console, keys: [[:attachments, :json, "tags"], [:meta, :source_url]]
+  outgoing Annoyer.Outgoing.Console, keys: [[:attachments, "json", "tags"], [:meta, :source_url]]
 end
--- lib/annoyer/transform/json_parser.ex
+++ lib/annoyer/transform/json_parser.ex
@@ -38,6 +38,6 @@ defmodule Annoyer.Transform.JsonParser do
   end
 
   defp handle_parsed(_params, parsed, annoyence) do
-    Path.put([:attachments, :json], annoyence, parsed)
+    Path.put([:attachments, "json"], annoyence, parsed)
   end
 end
